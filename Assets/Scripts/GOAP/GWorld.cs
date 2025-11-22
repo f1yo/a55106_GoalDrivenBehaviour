3deca88df5ef3bb08e36d1711ca94fd577eb379c
fly <Benylaranjinha@gmail.com>
2025-11-22 21:00:31 +0000

    Adding More Resources to the World

--- Assets/Scripts/GOAP/GWorld.cs
+++ Assets/Scripts/GOAP/GWorld.cs
@@ -1,24 +1,70 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public sealed class GWorld
-{
+public sealed class GWorld {
+
     private static readonly GWorld instance = new GWorld();
     private static WorldStates world;
+    private static Queue<GameObject> patients;
+    private static Queue<GameObject> cubicles;
 
-    static GWorld()
-    {
+    static GWorld() {
+
         world = new WorldStates();
+        patients = new Queue<GameObject>();
+        cubicles = new Queue<GameObject>();
+
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cubicle");
+        foreach (GameObject c in cubes)
+        {
+            cubicles.Enqueue(c);
+        }
+
+        if(cubes.Length > 0)
+        {
+            world.ModifyState("FreeCubicle", cubes.Length);
+        }
+
     }
 
-    private GWorld()
+    GWorld()
     {
+
     }
 
-    public static GWorld Instance
+    public void AddPatients(GameObject p)
     {
-        get { return instance; }
+        patients.Enqueue(p);
+    }
+
+    public GameObject RemovePatient()
+    {
+        if (patients.Count == 0) return null;
+        {
+            return patients.Dequeue();
+        }
+    }
+
+    public void AddCubicle(GameObject p)
+    {
+        cubicles.Enqueue(p);
+    }
+
+    public GameObject RemoveCubicle()
+    {
+        if (cubicles.Count == 0) return null;
+        {
+            return cubicles.Dequeue();
+        }
+    }
+
+    public static GWorld Instance 
+    { 
+        get 
+        { 
+            return instance; 
+        } 
     }
 
     public WorldStates GetWorld()
--- Assets/Scripts/GOAP/WorldStates.cs
+++ Assets/Scripts/GOAP/WorldStates.cs
@@ -1,61 +1,57 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class WorldState
-{
+public class WorldState {
+
     public string key;
     public int value;
 }
 
-public class WorldStates
-{
+public class WorldStates {
+
     public Dictionary<string, int> states;
 
-    public WorldStates()
-    {
+    public WorldStates() {
+
         states = new Dictionary<string, int>();
     }
 
-    public bool HasState(string key)
-    {
-        return states.ContainsKey(key);
-    }
+    bool HasState(string key) => states.ContainsKey(key);
 
-    void AddState(string key, int value)
-    {
+    void AddState(string key, int value) {
+
         states.Add(key, value);
     }
 
-    public void ModifyState(string key, int value)
-    {
-        if (states.ContainsKey(key))
-        {
+    void RemoveState(string key) {
+
+        if (HasState(key)) states.Remove(key);
+    }
+
+    public void ModifyState(string key, int value) {
+
+        if (HasState(key)) {
+
             states[key] += value;
-            if (states[key] <= 0)
-                RemoveState(key);
+            if (states[key] <= 0) RemoveState(key);
+        } else {
+
+            AddState(key, value);
         }
-        else
-            states.Add(key, value);
     }
 
-    public void RemoveState(string key)
-    {
-        if (states.ContainsKey(key))
-            states.Remove(key);
-    }
+    void SetState(string key, int value) {
 
-    public void SetState(string key, int value)
-    {
-        if (states.ContainsKey(key))
+        if (HasState(key)) {
+
             states[key] = value;
-        else
-            states.Add(key, value);
+        } else {
+
+            AddState(key, value);
+        }
     }
 
-    public Dictionary<string, int> GetStates()
-    {
-        return states;
-    }
+    public Dictionary<string, int> GetStates() => states;
 }
